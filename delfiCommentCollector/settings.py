95ed6aef64ecdec4bca6eb346521a428cdb52c91
Hain Zuppur <h.zuppur@gmail.com>
2020-09-30 22:25:27 +0300

    Changed setings to work on heroku

--- delfiCommentCollector/settings.py
+++ delfiCommentCollector/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'delfiCommentCollector.urls'
