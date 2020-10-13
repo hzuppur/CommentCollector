5e4f7f5749e625869c6ba1c8eeafb769b2605342
Hain Zuppur <h.zuppur@gmail.com>
2020-10-13 10:58:16 +0300

    Added remove duplicates func

--- commentCollector/management/commands/remove_duplicates.py
+++ commentCollector/management/commands/remove_duplicates.py
@@ -2,13 +2,18 @@ from django.core.management.base import BaseCommand, CommandError
 from commentCollector.models import SavedComment
 
 
-
 class Command(BaseCommand):
-  # python manage.py add_articles_to_table
-  help = 'Remove duplicates from saved coments'
+    # python manage.py add_articles_to_table
+    help = 'Remove duplicates from saved coments'
 
-  def handle(self, *args, **options):
-      # assuming which duplicate is removed doesn't matter...
-      for row in SavedComment.objects.all().reverse():
-          if SavedComment.objects.filter(content=row.content).count() > 1:
-              row.delete()
+    def handle(self, *args, **options):
+        for row in SavedComment.objects.all():
+            if "ĹÉÈXÍ.NÈŤ" in row.content or "M­­a­­g­­n­ifyIn­­c­­o­­m­e.C­OM":
+                row.delete()
+
+
+    def remove_duplicates(self):
+        # assuming which duplicate is removed doesn't matter...
+        for row in SavedComment.objects.all().reverse():
+            if SavedComment.objects.filter(content=row.content).count() > 1:
+                row.delete()
