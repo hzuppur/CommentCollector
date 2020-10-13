674c5b324e87aa24a33d3c831315cc11abb29934
Hain Zuppur <h.zuppur@gmail.com>
2020-10-13 10:21:53 +0300

    Added remove duplicates func

---
+++ commentCollector/management/commands/remove_duplicates.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+from commentCollector.models import SavedComment
+
+
+
+class Command(BaseCommand):
+  # python manage.py add_articles_to_table
+  help = 'Remove duplicates from saved coments'
+
+  def handle(self, *args, **options):
+      # assuming which duplicate is removed doesn't matter...
+      for row in SavedComment.objects.all().reverse():
+          if SavedComment.objects.filter(id=row.id).count() > 1:
+              row.delete()
